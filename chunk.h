29a8872033fde126c44c916aacec295ae373d93c
Johnathin <johnathinfooks@gmail.com>
2026-01-27 22:00:13 -0800

    Initial commit

---
+++ chunk.h
@@ -0,0 +1,16 @@
+#ifndef CLOX_CHUNK_H
+#define CLOX_CHUNK_H
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+#endif // CLOX_CHUNK_H
